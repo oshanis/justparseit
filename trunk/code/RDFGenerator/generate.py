b00215d7f4c8a1e3b11df2142e96c0b0e1677898
oshanis <oshanis@678b118e-f24a-0410-888d-6727524aa8be>
2008-05-04 21:30:45 +0000

    Structural changes to the RDFGenerator

--- trunk/code/RDFGenerator/generate.py
+++ trunk/code/RDFGenerator/generate.py
@@ -1,3 +1,9 @@
+"""
+6.863 Spring 2008 Project
+
+The RDF Generator
+"""
+
 import sys
 import logging
 
@@ -39,6 +45,7 @@ def fragIDs(components):
         new_components[x] = "#" + components[x].replace(" ","_") 
     return new_components
 
+
 def getMatch(term):
 
 	"""
@@ -51,53 +58,56 @@ def getMatch(term):
 	matches = []
 	for row in g.query('select ?a WHERE { ?a rdfs:label "'+ term +'"}',initNs=dict(rdfs=Namespace("http://www.w3.org/2000/01/rdf-schema#"))):
 		matches.append(row)
-	return matches.pop() #TODO: what is there are more than 1 match? :(
+	return matches[0] #TODO: what is there are more than 1 match?
     
-store = Graph()
+def constructPolicy():
+	store = Graph()
+	# Bind a few prefix, namespace pairs.
+	store.bind("air", "http://dig.csail.mit.edu/TAMI/2007/amord/air#")
+	store.bind("pur", "http://dig.csail.mit.edu/TAMI/2006/s4/purposes#")
+	store.bind("mit", "http://dig.csail.mit.edu/TAMI/2007/s0/university#")
+	store.bind("owl", "http://www.w3.org/2002/07/owl#")
 
-# Bind a few prefix, namespace pairs.
-store.bind("air", "http://dig.csail.mit.edu/TAMI/2007/amord/air#")
-store.bind("pur", "http://dig.csail.mit.edu/TAMI/2006/s4/purposes#")
-store.bind("mit", "http://dig.csail.mit.edu/TAMI/2007/s0/university#")
-store.bind("owl", "http://www.w3.org/2002/07/owl#")
+	# Create namespace objects.
+	AIR = Namespace("http://dig.csail.mit.edu/TAMI/2007/amord/air#")
+	PUR = Namespace("http://dig.csail.mit.edu/TAMI/2006/s4/purposes#")
+	MIT = Namespace("http://dig.csail.mit.edu/TAMI/2007/s0/university#")
+	OWL = Namespace("http://www.w3.org/2002/07/owl#")
+	
+	f = fragIDs(parseNL())
+	# create the policy
+	policy = URIRef(f['POLICY'])
+	store.add((policy, RDF.type, AIR["Policy"]))
 
-# Create namespace objects.
-AIR = Namespace("http://dig.csail.mit.edu/TAMI/2007/amord/air#")
-PUR = Namespace("http://dig.csail.mit.edu/TAMI/2006/s4/purposes#")
-MIT = Namespace("http://dig.csail.mit.edu/TAMI/2007/s0/university#")
-OWL = Namespace("http://www.w3.org/2002/07/owl#")
+	# add the variables
+	store.add((policy, AIR["variable"], URIRef("#U")))
+	store.add((policy, AIR["variable"], URIRef("#D")))
+	store.add((policy, AIR["variable"], URIRef("#P")))
 
-f = fragIDs(parseNL())
-# create the policy
-policy = URIRef(f['POLICY'])
-store.add((policy, RDF.type, AIR["Policy"]))
+	# add the outer rule
+	outer_rule = BNode()
+	store.add((policy, AIR["rule"], outer_rule))
+	
+	outer_rule_pattern = Graph()
+	outer_rule_pattern.add((URIRef("#U"), RDF.type, AIR["UseEvent"]))
+	outer_rule_pattern.add((URIRef("#U"), AIR["data"], URIRef("#D")))
+	outer_rule_pattern.add((URIRef("#U"), AIR["purpose"], URIRef("#P")))
+	outer_rule_pattern.add((URIRef("#D"), RDF.type, MIT["ProxCardEvent"]))
 
-#add the variables
-store.add((policy, AIR["variable"], URIRef("#U")))
-store.add((policy, AIR["variable"], URIRef("#D")))
-store.add((policy, AIR["variable"], URIRef("#P")))
+	store.add((policy, AIR["pattern"], outer_rule_pattern))
 
-#add the outer rule
-outer_rule = BNode()
-store.add((policy, AIR["rule"], outer_rule))
+	# create the rule body
+	store.add((outer_rule, AIR["label"], Literal("MIT prox-card policy")))
+	store.add((outer_rule, AIR["pattern"], Literal("MIT prox-card policy")))
 
-outer_rule_pattern = Graph()
-outer_rule_pattern.add((URIRef("#U"), RDF.type, AIR["UseEvent"]))
-outer_rule_pattern.add((URIRef("#U"), AIR["data"], URIRef("#D")))
-outer_rule_pattern.add((URIRef("#U"), AIR["purpose"], URIRef("#P")))
-outer_rule_pattern.add((URIRef("#D"), RDF.type, MIT["ProxCardEvent"]))
+	# Serialize and save the result
+	store.serialize("policy.n3", format="n3")
 
-store.add((policy, AIR["pattern"], outer_rule_pattern))
+	# Serialize as N3
+	print store.serialize(format="n3")
 
-#create the rule body
-store.add((outer_rule, AIR["label"], Literal("MIT prox-card policy")))
-store.add((outer_rule, AIR["pattern"], Literal("MIT prox-card policy")))
+	a = "proximity card data"
+	print getMatch(a)
 
-# Serialize and save the result
-store.serialize("policy.n3", format="n3")
-
-# Serialize as N3
-print store.serialize(format="n3")
-
-a = "proximity card data"
-print getMatch(a)
+if __name__ == '__main__':
+    constructPolicy()
