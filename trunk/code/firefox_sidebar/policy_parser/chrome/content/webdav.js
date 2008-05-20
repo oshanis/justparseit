b3e24f34899f5d208ae64a2f738da2b73db58c62
oshanis <oshanis@678b118e-f24a-0410-888d-6727524aa8be>
2008-05-20 04:27:36 +0000

    add webdav.js

---
+++ trunk/code/firefox_sidebar/policy_parser/chrome/content/webdav.js
@@ -0,0 +1,18 @@
+var store_location = "http://dig.csail.mit.edu/2008/webdav/policy.n3"; //the default value
+
+function store()
+{
+	store_location = document.getElementById("store_location").value;
+	webdav.manager.register(store_location, success);
+	webdav.manager.save_file(store_location, request1.responseText, fetch);
+}
+
+function fetch()
+{
+	content.window.location.replace(store_location,true);
+}
+
+function success()
+{
+        //TODO: Do something on the callback?
+}
